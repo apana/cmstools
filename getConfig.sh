62a3d0c46965a9bd21dcf7854aacd047151fe422
Leonard Apanasevich <apana@fnal.gov>
2014-12-05 10:51:20 -0600

    add DataOrMC option

--- getConfig.sh
+++ getConfig.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+echo "XXX"
 menu=/dev/CMSSW_4_2_0/GRun/V16
 # menu=/dev/CMSSW_4_2_0/GRun/V13
 globalTag=GR_H_V16::All
 ProcessName=HLTGRun
 CfgName=offline_xxx.py
-hltGetConfiguration ${menu} --full --offline --data --unprescale --globaltag ${globalTag} --process ${ProcessName} > ${CfgName}
 
+DataOrMC=MC
 
-
+if [ $DataOrMC == "MC" ]
+then
+    hltGetConfiguration ${menu} --full --offline --mc --unprescale --globaltag ${globalTag} --process ${ProcessName} > ${CfgName}
+else
+    hltGetConfiguration ${menu} --full --offline --data --unprescale --globaltag ${globalTag} --process ${ProcessName} > ${CfgName}
+fi
