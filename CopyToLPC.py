f6b4fddd38b5474da99f17679639216aa790625a
Leonard Apanasevich <apana@fnal.gov>
2016-10-28 12:31:32 -0500

    Update macro

--- CopyToLPC.py
+++ CopyToLPC.py
@@ -8,7 +8,7 @@
 #
 # Description :
 
-
+import sys
 import subprocess
 import subprocess
 from multiprocessing import Pool
@@ -17,16 +17,35 @@ from multiprocessing import Pool
 # filename = "r275059_itgv63p1.list"
 # filename = "r275064_itgv63p1.list"
 # filename = "r279931_v86SK.list"
-filename = "279931_ParkingZeroBias.list"
+# filename = "279931_ParkingZeroBias.list"
 # filename = "testing.list"
 
-newfile=filename + ".new"
 
 def fwork(cmd):
     return subprocess.call(cmd, shell=True)
 
+def usage():
+    """ Usage: CopyToLPC filelist <nproc>
+\n\tPlease supply filelist
+    """
+    pass
+
 if __name__ == "__main__":
-    p = Pool(8)
+
+    nproc=8
+    
+    narg=len(sys.argv)
+    if narg < 2 :
+        print usage.__doc__
+        sys.exit(1)
+    if narg >=3:
+        nproc=sys.arv[2]
+    
+    filename = sys.argv[1]
+    newfile=filename + ".new"
+        
+    p = Pool(nproc)
+    
     cmdlist = []
     # f = open("r259721_itgv46p0_local.list", "w")
     # with open("r259721_itgv46p0.list") as file:
@@ -34,11 +53,16 @@ if __name__ == "__main__":
     with open("%s" % filename) as file:
         for line_ in file.readlines():
             line =line_.strip()
-            print line
+            print line, "  xx"
             ##newloc = line.replace("eoscms.cern.ch", "cmseos.fnal.gov")
             ##newloc = newloc.replace("dpg_trigger", "lpctrig")
-            newloc = line.replace("cmseos.fnal.gov//eos/uscms","eoscms.cern.ch//eos/cms")
-            newloc = newloc.replace("user/lpctrig/apana/L1Menu_2016","group/dpg_trigger/comm_trigger/L1Trigger/L1Menu2016")
+            
+            ## newloc = line.replace("cmseos.fnal.gov//eos/uscms","eoscms.cern.ch//eos/cms")
+            ## newloc = newloc.replace("user/lpctrig/apana/L1Menu_2016","group/dpg_trigger/comm_trigger/L1Trigger/L1Menu2016")
+
+            newloc = line.replace("cmseos.fnal.gov//store","eoscms.cern.ch//eos/cms/store")
+            newloc = newloc.replace("user//lpctrig/apana/L1Menu_2016","group/dpg_trigger/comm_trigger/L1Trigger/L1Menu2016")
+            
             f.write(newloc+"\n")
             print newloc,"\n"
             cmdlist.append("xrdcp %s %s" % ( line, newloc ))
